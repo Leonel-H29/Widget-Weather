fc4cb043070243fb7e7a996f625ab8771b01f616
Leonel-H29 <leonelherreradkno@gmail.com>
2023-01-30 20:18:07 -0300

    Agrego un mapa para la ubicacion

--- widget/src/components/WeatherInfo.js
+++ widget/src/components/WeatherInfo.js
@@ -12,7 +12,20 @@ export default function WeatherInfoComp({ weather }) {
           />
         </div>
       </div>
-      <div>{weather?.location.name}</div>
+      <div>
+        <div>{weather?.current.condition.text}</div>
+        <div>{weather?.current.temp_c}°</div>
+      </div>
+      <iframe
+        title="mapa"
+        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d28475.847806771875!2d${weather?.location.lon}9!3d${weather?.location.lat}8!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1675119655081!5m2!1ses!2sar`}
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
