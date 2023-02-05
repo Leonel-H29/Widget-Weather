3b14af33c22afc118b0984189ceec20470afb62b
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-05 11:44:55 -0300

    Agrego la paginacion en el listado de las horas de los demas dias

---
+++ widget/src/components/WeatherInfoForHour.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import moment from 'moment/moment';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Paper from '@mui/material/Paper';
+import { ListSubheader } from '@mui/material';
+import { Box, Pagination } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+const pageSize = 6;
+export default function HourInfoComp({ weather }) {
+  const [weather_hour, setWeather_Hour] = useState([]);
+  const [pagination, setPagination] = useState({
+    count: 0,
+    from: 0,
+    to: pageSize,
+  });
+  const service = {
+    getData: ({ from, to }) => {
+      //console.log(weather);
+      return new Promise((resolve, reject) => {
+        const data = weather?.hour.slice(from, to);
+        resolve({
+          count: weather?.hour.length,
+          data: data,
+        });
+      });
+    },
+  };
+  /*Obtengo los datos para cada una de las paginas */
+  function getData() {
+    service
+      .getData({ from: pagination.from, to: pagination.to })
+      .then((response) => {
+        console.log(response);
+        setPagination({ ...pagination, count: response.count });
+        setWeather_Hour(response.data);
+      });
+  }
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pagination.from, pagination.to]);
+
+  /*Cambio de pagina */
+  const handleChangePage = (event, page) => {
+    const from = (page - 1) * pageSize;
+    const to = (page - 1) * pageSize + pageSize;
+    setPagination({ ...pagination, from: from, to: to });
+  };
+
+  /*Menu de Paginacion */
+  const menu_pagination = (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Pagination
+        count={Math.ceil(pagination.count / pageSize)}
+        onChange={handleChangePage}
+      />
+    </Box>
+  );
+  /*Constante de la lista de tiempo por hora */
+  const ListHourW = weather_hour.map((hora) => (
+    <List
+      key={hora.time}
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+        position: 'relative',
+      }}
+    >
+      <ListSubheader>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar
+              alt="Remy Sharp"
+              src={hora.condition.icon}
+              title={hora.condition.text}
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary={moment(hora.time).format('LT')}
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  <b>{hora.temp_c}</b> °&nbsp;
+                </Typography>
+                - {hora.condition.text}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </ListSubheader>
+      <Divider variant="inset" component="li" />
+    </List>
+  ));
+
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>View more details: </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Paper square sx={{ pb: '50px' }}>
+            {ListHourW}
+            {menu_pagination}
+          </Paper>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
---
+++ widget/src/components/WeatherInfoModal.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import moment from 'moment/moment';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { CardActions } from '@mui/material';
+import Button from '@mui/material/Button';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Paper from '@mui/material/Paper';
+import { ListSubheader } from '@mui/material';
+import HourInfoComp from './WeatherInfoForHour';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  Overflow: 'scroll',
+  p: 4,
+};
+export default function ModalInfoComp({ weather }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <CardActions>
+        <Button size="small" onClick={handleOpen}>
+          VIEW MORE
+        </Button>
+      </CardActions>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <HourInfoComp weather={weather} />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
