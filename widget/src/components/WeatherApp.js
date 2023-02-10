96ac5f6f6948b1f35cfe58fe8fbd34674f4270fa
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-10 11:35:50 -0300

    Agrego el boton de almacenar la busqueda de la ciudad

--- widget/src/components/WeatherApp.js
+++ widget/src/components/WeatherApp.js
@@ -1,21 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import WeatherFormComp from './WeatherForm';
 import WeatherInfoComp from './WeatherInfo';
-import { Container } from '@mui/material';
+import { Container, Button } from '@mui/material';
 import ResponsiveAppBar from './MainMenu';
 import axios from 'axios';
 import Loadding from './Loadding';
 import Error from './Error';
 import Footer from './Footer';
 import { FormattedMessage } from 'react-intl';
+import SaveIcon from '@mui/icons-material/Save';
 import OptionsLangComp from './OptionsLanguages';
-//import traslateServ from '../services/traslate-service';
+import { If, Then } from 'react-if-elseif-else-render';
 const lang = localStorage.getItem('lang');
 const API_WEATHER_URL =
   process.env.REACT_APP_URL + '&key=' + process.env.REACT_APP_KEY + '&q=';
 export default function WeatherApiComp() {
   const [weather, setWeather] = useState(null);
   const [fail, setFail] = useState(null);
+  const [btn_save, setBtn_Save] = useState(true);
+  let default_city = localStorage.getItem('city');
+  /*
+  En caso de que no se encuentre ninguna ciudad guardada, 
+  por defecto se mostrara la informacion de Londres
+  */
+  if (!default_city) default_city = 'london';
 
   //Muestra la ciudad cargada por defecto
   useEffect(() => {
@@ -29,7 +38,7 @@ export default function WeatherApiComp() {
   }, [weather]);
 
   //Cargo los datos del clima segun la ciudad
-  async function loadInfo(city = 'london') {
+  async function loadInfo(city = default_city) {
     //console.log(API_WEATHER_URL + city);
     var API_URL = API_WEATHER_URL + city;
     if (lang) {
@@ -41,6 +50,7 @@ export default function WeatherApiComp() {
         .then((response) => {
           //console.log(response.data);
           setWeather(response.data);
+          setBtn_Save(false);
         })
         .catch((error) => {
           setFail(error);
@@ -52,10 +62,27 @@ export default function WeatherApiComp() {
 
   function handleChangeCity(city) {
     setWeather(null);
+    setBtn_Save(true);
     setTimeout(() => {
       loadInfo(city);
     }, 3000);
   }
+  //Boton para gurdar la ciudad buscada en el almacenamiento local
+  const SaveCity = (
+    <Button
+      variant="contained"
+      disabled={btn_save}
+      onClick={() => {
+        localStorage.setItem(
+          'city',
+          weather?.location.name + ' ' + weather?.location.country
+        );
+      }}
+    >
+      <SaveIcon />
+      SAVE CITY
+    </Button>
+  );
 
   return (
     <div>
@@ -79,6 +106,12 @@ export default function WeatherApiComp() {
             />
           </h3>
           <WeatherFormComp onChangeCity={handleChangeCity} />
+          <br />
+          <If condition={fail == null}>
+            <Then>{SaveCity}</Then>
+          </If>
+
+          <br />
           <br />
           {/*
         Si se encuentra una falla muestrara el mensaje de error.
