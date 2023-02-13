c68aff0974fbd54f93e2225f60547ed3a35beb9a
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-13 12:42:23 -0300

    Agrego boton de busqueda por voz y lo ubico al lado del Textfield en caso de que sea reconocido por el navegador

--- widget/src/components/WeatherForm.js
+++ widget/src/components/WeatherForm.js
@@ -5,13 +5,20 @@ import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import SpeechRecognition, {
   useSpeechRecognition,
 } from 'react-speech-recognition'; // Importar el hook useSpeechRecognition desde 'react-speech-recognition'
-
+import { Else, If, Then } from 'react-if-elseif-else-render';
 const LabelForm = (
   <FormattedMessage id="app.label-form" defaultMessage="City" />
 );
 export default function WeatherFormComp({ onChangeCity }) {
   const [city, setCity] = useState('');
   const [fail, setFail] = useState(null);
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition({});
 
   useEffect(() => {
     if (city.length >= 0 && city.length === 2)
@@ -31,34 +38,71 @@ export default function WeatherFormComp({ onChangeCity }) {
     e.preventDefault();
     onChangeCity(city);
   }
+
+  const startListening = () => {
+    resetTranscript();
+    //setMessage();
+    SpeechRecognition.startListening({
+      // Comandos no funcionan con es-HN
+      // language: "es-HN",
+      continuous: true,
+    });
+  };
+
+  const stopListening = () => {
+    SpeechRecognition.stopListening();
+  };
+
+  const InputSearch = (
+    <TextField
+      error={city.length >= 0 && city.length === 2}
+      id="filled-basic"
+      label={LabelForm}
+      variant="outlined"
+      onChange={onChange}
+      fullWidth={true}
+      required
+      helperText={fail}
+    />
+  );
+
+  const ButtonVoiceMicro = (
+    <Grid item xs={12} sm={3}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={SpeechRecognition.startListening}
+        startIcon={<KeyboardVoiceIcon />}
+        onMouseDown={startListening}
+        onMouseUp={stopListening}
+        onTouchStart={startListening}
+        onTouchEnd={stopListening}
+        onTouchCancel={stopListening}
+      >
+        Voice Search
+      </Button>{' '}
+    </Grid>
+  );
   return (
     <div>
       <Container>
         <form onSubmit={handleSubmit}>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12} sm={9}>
-              <TextField
-                error={city.length >= 0 && city.length === 2}
-                id="filled-basic"
-                label={LabelForm}
-                variant="outlined"
-                onChange={onChange}
-                fullWidth={true}
-                required
-                helperText={fail}
-              />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={SpeechRecognition.startListening}
-                startIcon={<KeyboardVoiceIcon />}
-              >
-                Voice Search
-              </Button>{' '}
-            </Grid>
-          </Grid>
+          {/*
+          En caso de que el reconocimiento de voz sea reconocido por el navegador, ubico en
+          boton de busqueda por voz al lado del Textfield, caso contrario solo aparecera el
+          Textfield 
+          */}
+          <If condition={browserSupportsSpeechRecognition}>
+            <Then>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item xs={12} sm={9}>
+                  {InputSearch}
+                </Grid>
+                {ButtonVoiceMicro}
+              </Grid>
+            </Then>
+            <Else>{InputSearch}</Else>
+          </If>
         </form>
       </Container>
     </div>
